51e46f499866a238d557d7cab7b126945a1e6a8e
GameHunter101 <liorscarmeli@gmail.com>
2023-09-05 15:33:45 -0400

    Trying to add alpha blending

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -129,7 +129,14 @@ impl Pipeline {
                 entry_point: "main",
                 targets: &[Some(wgpu::ColorTargetState {
                     format: color_format,
-                    blend: Some(wgpu::BlendState::REPLACE),
+                    blend: Some(wgpu::BlendState {
+                        color: wgpu::BlendComponent {
+                            src_factor: wgpu::BlendFactor::SrcAlpha,
+                            dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha,
+                            operation: wgpu::BlendOperation::Add,
+                        },
+                        alpha: wgpu::BlendComponent::OVER,
+                    }),
                     write_mask: wgpu::ColorWrites::ALL,
                 })],
             }),
